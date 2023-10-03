0508250df13abf666fb0d08016fcbd3b3447a8bb
Max <ibroden@yandex.ru>
2023-10-03 20:48:58 +0300

    some correctives

--- week04/ex3.sh
+++ week04/ex3.sh
@@ -3,7 +3,30 @@ gcc ex3.c -o ex3
 # n=3
 ./ex3 3 &
 
-sleep 7
+sleep 5
+pstree | grep ex3
+sleep 5
+pstree | grep ex3
+sleep 5
+pstree | grep ex3
+
+
+sleep 5
 
 # n=5
 ./ex3 5 &
+sleep 5
+pstree | grep ex3
+
+sleep 5
+pstree | grep ex3
+
+sleep 5
+pstree | grep ex3
+
+sleep 5
+pstree | grep ex3
+
+sleep 5
+pstree | grep ex3
+
