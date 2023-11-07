6942019d4f502f469731af6b60bff2ab8bef24ba
Max <ibroden@yandex.ru>
2023-11-07 19:27:02 +0300

    second task

--- week09/mmu.c
+++ week09/mmu.c
@@ -10,6 +10,7 @@
 
 #define FILENAME "/tmp/ex2/pagetable"
 #define FILENAME_2 "pagetable"
+#define MIN_BUFFER_SIZE 2
 
 typedef struct PTE {
 // The page is in the physical memory (RAM)
@@ -26,9 +27,50 @@ typedef struct PTE {
     int nfu_counter;
 } PTE;
 
+typedef struct TLB_entry {
+    int page;
+    int frame;
+} TLB;
+
+unsigned int buffer_size;
+TLB* buffer;
 unsigned int references = 0;
 unsigned int misses = 0;
+unsigned int TLB_miss = 0;
 short algo_type;
+
+int TLB_map(const int page) {
+    int frame = -1;
+    int idx_frame = -1;
+    /*
+     * In this cycle we try to get the most recent translation of that page (it is the most right).
+     */
+    for (int i = 0; i < buffer_size; i++) {
+        if (buffer[i].page == page) {
+            idx_frame = i;
+            frame = buffer[i].frame;
+        }
+    }
+    printf("found frame %d\n", frame);
+    if (frame == -1)
+        return -1;
+    /*
+     * And then in this cycle we try to see whether this frame wasn't allocated for some other page
+     */
+    for (idx_frame+=1; idx_frame < misses%buffer_size; idx_frame++) {
+        if (buffer[idx_frame].frame == frame) {  // if we got frame match, then it is a different page, since we already
+            printf("but it's been replaced by page %d, so it is no longer in memory\n", buffer[idx_frame].page);
+            return -1;  // had the rightest frame allocation of that page.
+        }
+    }
+
+    return frame;
+}
+
+void update_TLB(const int page, const int frame) {
+    buffer[misses%buffer_size].page = page;
+    buffer[misses%buffer_size].frame = frame;
+}
 
 void operation_preamble(char op_type, int page, bool valid) {
     /*
@@ -53,6 +95,7 @@ void operation_preamble(char op_type, int page, bool valid) {
 
 void benchmarking_report() {
     printf("Hit ratio: %lf\n", 1 - (double)misses/(double)references);
+    printf("TLB miss ratio: %lf\n", (double)TLB_miss/(double)references);
 }
 
 void ptable_report(PTE* ptable, int pt_size) {
@@ -60,6 +103,12 @@ void ptable_report(PTE* ptable, int pt_size) {
     for (int i = 0; i < pt_size; i++) {
         printf("Page %d ---> valid=%d, frame=%d, dirty=%d, referenced=%d, counter=%d\n", i, ptable[i].valid, ptable[i].frame,
                ptable[i].dirty, ptable[i].referenced, ptable[i].counter);
+    }
+}
+
+void print_tlb() {
+    for (int i = 0; i < buffer_size; i++) {
+        printf("page %d: frame %d\n", buffer[i].page, buffer[i].frame);
     }
 }
 
@@ -108,8 +157,23 @@ int main(int argc, char* argv[]) {
     }
     PTE* ptable = (PTE*)addr;
 
+    /*
+     * Buffer instantiation
+     */
+    buffer_size = pages * .2;
+    if (buffer_size == 0)
+        buffer_size = MIN_BUFFER_SIZE;
+
+    printf("%d\n", buffer_size);
+    buffer = malloc(sizeof(TLB) * buffer_size);
+    for (int i = 0; i < buffer_size; i++) {
+        buffer[i].frame = -1;
+        buffer[i].page = -1;
+    }
+
     char op_type;
     int page_n;
+    int tlb_result;
 
     int idx = 2;
     while (idx < argc-1) {
@@ -134,13 +198,24 @@ int main(int argc, char* argv[]) {
             ptable[page_n].referenced = 0;
         }
 
+        if ((tlb_result = TLB_map(page_n)) == -1) {
+            /*
+             * No such frame found in TLB.
+             */
+            printf("TLB miss\n");
+            print_tlb();
+            TLB_miss++;
+        }
+
         if (!ptable[page_n].valid) {
             ptable[page_n].referenced = getpid();
             printf("Ask pager to load it from disk (SIGUSR1 signal) and wait\n");
 
             kill(pager_pid, SIGUSR1);
             pause();
+            update_TLB(page_n, ptable[page_n].frame);
         }
+
         if (op_type == 'W') {
             printf("It is a write request then set the dirty field\n");
             ptable[page_n].dirty = true;
@@ -148,7 +223,6 @@ int main(int argc, char* argv[]) {
         if (op_type == 'R') {
 
         }
-//        ptable_report(ptable, pages);
     }
 
     if (munmap(addr, sz) == -1) {
