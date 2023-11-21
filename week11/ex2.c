d31cabf524f92ba287e037aad991ef4a5ed9d0a7
Max <ibroden@yandex.ru>
2023-11-21 20:39:30 +0300

    second task

--- week11/ex2.c
+++ week11/ex2.c
@@ -15,7 +15,7 @@
 #define USED 1
 #define UNUSED 0
 
-typedef struct inode {
+typedef struct __attribute__((__packed__)) {
     char name[FILENAME_SIZE]; // file name 16
     int size; // file size (in number of blocks) 4 20
     int blockPointers[BLOCKS_PER_FILE]; // direct block pointers 4 * 8 = 32 52
@@ -31,45 +31,24 @@ FILE* fs;
 
 void parse_free_block(char* buffer) {
     fseek(fs, 0, SEEK_SET);
-    printf("%ld\n", ftell(fs));
     fread(buffer, sizeof(char), N_BLOCKS, fs);
 }
 
 void parse_inode_block(inode* buffer) {
     fseek(fs, N_BLOCKS, SEEK_SET);
-    printf("%ld\n", ftell(fs));
     for (int i = 0; i < N_FILES; i++) {
-        fread(&buffer[i].name, sizeof(char), FILENAME_SIZE, fs);
-        fscanf(fs, "%d", &buffer[i].size);
-        for (int b = 0; b < BLOCKS_PER_FILE; b++) {
-            fscanf(fs, "%d", &buffer[i].blockPointers[b]);
-        }
-        fscanf(fs, "%d", &buffer[i].used);
+        fread(&buffer[i], sizeof(inode), 1, fs);
     }
 }
 
 void commit_free_block(char* buffer) {
     fseek(fs, 0, SEEK_SET);
-    for (int block = 0; block < N_BLOCKS; block++) {
-        fprintf(fs, "%c", buffer[block]);
-    }
+    fwrite(buffer, sizeof(char), N_BLOCKS, fs);
 }
 
 void commit_inode_block(inode* buffer) {
     fseek(fs, N_BLOCKS, SEEK_SET);
-    for (int i = 0; i < N_FILES; i++) {
-        fprintf(fs, "%s", buffer[i].name);
-        // if we got less than 16 bytes string, then just append zeros to the end to fulfill the length.
-        for (int i = 0; i < FILENAME_SIZE - 1 - strlen(buffer[i].name); i++) {
-            fprintf(fs, "%c", 0);
-        }
-
-        fprintf(fs, "%d", buffer[i].size);
-        for (int b = 0; b < BLOCKS_PER_FILE; b++) {
-            fprintf(fs, "%d", buffer[i].blockPointers[b]);
-        }
-        fprintf(fs, "%d", buffer[i].used);
-    }
+    fwrite(buffer, sizeof(inode), N_FILES, fs);
 }
 
 void perform_reading(char buf[BLOCK_SIZE], int address) {
@@ -96,12 +75,6 @@ int locate_inode_by_name(const inode* inode_list, const char* name) {
 int create(char name[FILENAME_SIZE], int size){
     /* create a file with this name and this size */
 
-    // Step 1: check to see if we have sufficient free space on disk by
-    // reading in the free block list. To do this:
-    // move the file pointer to the start of the disk file.
-    // Read the first 128 bytes (the free/in-use block information)
-    // Scan the list to make sure you have sufficient free blocks to
-    // allocate a new file of this size
     int pntrs[BLOCKS_PER_FILE];
     int cur = 0;
     char fb_list[N_BLOCKS];
@@ -123,13 +96,6 @@ int create(char name[FILENAME_SIZE], int size){
        return EXIT_FAILURE;
    }
 
-    // Step 2: we look  for a free inode on disk
-    // Read in an inode
-    // check the "used" field to see if it is free
-    // If not, repeat the above two steps until you find a free inode
-    // Set the "used" field to 1
-    // Copy the filename to the "name" field,
-    // Copy the file size (in units of blocks) to the "size" field
     inode inode_list[N_FILES];
     parse_inode_block(inode_list);
 
@@ -145,24 +111,11 @@ int create(char name[FILENAME_SIZE], int size){
     inode_list[free_inode].size = size;
     strcpy(inode_list[free_inode].name, name);
 
-    // Step 3: Allocate data blocks to the file
-    // for(i=0;i<size;i++)
-    // Scan the block list that you read in Step 1 for a free block
-    // Once you find a free block, mark it as in-use (Set it to 1)
-    // Set the blockPointer[i] field in the inode to this block number.
-    //
-    // end for
     for (int i = 0; i < size; i++) {
         fb_list[pntrs[i]] = USED;
         inode_list[free_inode].blockPointers[i] = pntrs[i];
     }
 
-
-    // Step 4: Write out the inode and free block list to disk
-    // Move the file pointer to the start of the file
-    // Write out the 128 byte free block list
-    // Move the file pointer to the position on disk where this inode was stored
-    // Write out the inode
     commit_free_block(fb_list);
     commit_inode_block(inode_list);
 
@@ -172,13 +125,6 @@ int create(char name[FILENAME_SIZE], int size){
 int delete(char name[FILENAME_SIZE]){
     // Delete the file with this name
 
-    // Step 1: Locate the inode for this file
-    // Move the file pointer to the 1st inode (129th byte)
-    // Read in a inode
-    // If the inode is free, repeat above step.
-    // If the inode is in use, check if the "name" field in the
-    // inode matches the file we want to delete. IF not, read the next
-    // inode and repeat
     char fb_list[N_BLOCKS];
     memset(fb_list, 0, N_BLOCKS);
 
@@ -194,11 +140,6 @@ int delete(char name[FILENAME_SIZE]){
         return EXIT_FAILURE;
     }
 
-    // Step 2: free blocks of the file being deleted
-    // Read in the 128 byte free block list (move file pointer to start of the disk and read in 128 bytes)
-    // Free each block listed in the blockPointer fields as follows:
-    // for(i=0;i< inode.size; i++)
-    // freeblockList[ inode.blockPointer[i] ] = 0;
     for (int i = 0; i < inode_list[idx].size; i++) {
         fb_list[inode_list[idx].blockPointers[i]] = UNUSED;
     }
@@ -210,11 +151,6 @@ int delete(char name[FILENAME_SIZE]){
     strcpy(inode_list[idx].name, "");
 
 
-    // Step 4: Write out the inode and free block list to disk
-    // Move the file pointer to the start of the file
-    // Write out the 128 byte free block list
-    // Move the file pointer to the position on disk where this inode was stored
-    // Write out the inode
     commit_free_block(fb_list);
     commit_inode_block(inode_list);
 
@@ -224,12 +160,6 @@ int delete(char name[FILENAME_SIZE]){
 int ls(void){
     // List names of all files on disk
 
-    // Step 1: read in each inode and print!
-    // Move file pointer to the position of the 1st inode (129th byte)
-    // for(i=0;i<16;i++)
-    // REad in a inode
-    // If the inode is in-use
-    // print the "name" and "size" fields from the inode
     inode inode_list[N_FILES];
 
     parse_inode_block(inode_list);
@@ -243,14 +173,6 @@ int ls(void){
 
 int read_(char name[FILENAME_SIZE], int blockNum, char buf[BLOCK_SIZE]){
 
-    // read this block from this file
-
-    // Step 1: locate the inode for this file
-    // Move file pointer to the position of the 1st inode (129th byte)
-    // Read in a inode
-    // If the inode is in use, compare the "name" field with the above file
-    // IF the file names don't match, repeat
-
     inode inode_list[N_FILES];
 
     parse_inode_block(inode_list);
@@ -261,11 +183,6 @@ int read_(char name[FILENAME_SIZE], int blockNum, char buf[BLOCK_SIZE]){
         return EXIT_FAILURE;
     }
 
-    // Step 2: Read in the specified block
-    // Check that blockNum < inode.size, else flag an error
-    // Get the disk address of the specified block
-    // That is, addr = inode.blockPointer[blockNum]
-    // move the file pointer to the block location (i.e., to byte # addr*1024 in the file)
     if (blockNum >= inode_list[idx].size) {
         printf("Block is too big to belong to a file !\n");
         return EXIT_FAILURE;
@@ -280,13 +197,6 @@ int read_(char name[FILENAME_SIZE], int blockNum, char buf[BLOCK_SIZE]){
 
 int write_(char name[FILENAME_SIZE], int blockNum, char buf[BLOCK_SIZE]){
 
-    // write this block to this file
-
-    // Step 1: locate the inode for this file
-    // Move file pointer to the position of the 1st inode (129th byte)
-    // Read in a inode
-    // If the inode is in use, compare the "name" field with the above file
-    // IF the file names don't match, repeat
     inode inode_list[N_FILES];
 
     parse_inode_block(inode_list);
@@ -297,11 +207,6 @@ int write_(char name[FILENAME_SIZE], int blockNum, char buf[BLOCK_SIZE]){
         return EXIT_FAILURE;
     }
 
-    // Step 2: Write to the specified block
-    // Check that blockNum < inode.size, else flag an error
-    // Get the disk address of the specified block
-    // That is, addr = inode.blockPointer[blockNum]
-    // move the file pointer to the block location (i.e., byte # addr*1024)
     if (blockNum >= inode_list[idx].size) {
         printf("Block is too big to belong to a file !\n");
         return EXIT_FAILURE;
@@ -321,7 +226,6 @@ int main(int argc, char *argv[]){
 
     disk_name = argv[1];
     fs = fopen(disk_name, "r+");
-    fprintf(fs, "%c", 1);
 
     char buffer[BLOCK_SIZE];
     for (int idx = 2; idx < argc; idx++) {
--- week11/ex2.sh
+++ week11/ex2.sh
@@ -2,4 +2,5 @@ gcc create_fs.c -o create_fs
 gcc ex2.c -o implementation
 
 ./create_fs disk0
-./implementation cat input.txt
+
+./implementation $(cat input.txt)
